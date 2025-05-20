c5712ae06fa0a0be15ffaef4e17ee217371f1170
Methankrieg <wolf.r.becker@googlemail.com>
2025-05-20 20:59:06 +0200

    Add files via upload

---
+++ 04_Assets/Karten/HTML_export/karte_marker_v2.js
@@ -0,0 +1,11 @@
+const markerTemplates = {
+  "arkoniden_standard": "<div class='marker arkoniden_standard'>...</div>",
+  "arkoniden_admiral": "<div class='marker arkoniden_admiral'>...</div>",
+  "arkoniden_raumfort": "<div class='marker arkoniden_raumfort'>...</div>",
+  "maahks_standard": "<div class='marker maahks_standard'>...</div>",
+  "maahks_jaeger": "<div class='marker maahks_jaeger'>...</div>",
+  "maahks_traeger": "<div class='marker maahks_traeger'>...</div>",
+  "maahks_admiral": "<div class='marker maahks_admiral'>...</div>"
+};
+
+// TODO: Hier folgt später die Platzierungslogik für die Marker auf der Karte
